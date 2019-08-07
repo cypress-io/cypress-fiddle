80d1a9385b6f6f29150cb2179b9cf92a3fab8eba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-08-07 14:15:21 -0400

    use highlight js

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,24 +2,80 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
 
-context('Example Cypress TodoMVC test', () => {
-  beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
-    cy.visit('http://todomvc.com/examples/vue/')
+import { source } from 'common-tags'
+
+Cypress.Commands.add('runExample', (html, test) => {
+  const appHtml = `
+    <head>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.9/styles/github.min.css">
+      <script charset="UTF-8" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.9/highlight.min.js"></script>
+      <script charset="UTF-8" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.9/languages/javascript.min.js"</script>
+      <script charset="UTF-8" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.9/languages/html.min.js"</script>
+      <script>hljs.initHighlightingOnLoad();</script>
+    </head>
+    <body>
+      <h1>Test code</h1>
+
+      <pre><code class="javascript">${test}</code></pre>
+
+      <h1>HTML</h1>
+      <div id="html">
+      <pre><code class="html">${Cypress._.escape(html)}</code></pre>
+      </div>
+
+      <h1>Live HTML</h1>
+      <div id="live">
+      ${html}
+      </div>
+    </body>
+  `
+  const document = cy.state('document')
+  document.write(appHtml)
+  document.close()
+
+  const noLog = { log: false }
+  cy.get('#live', noLog).within(noLog, () => {
+    eval(test)
+  })
+})
+
+context('Cypress example', () => {
+  describe('.get()', () => {
+    it('.get()', function () {
+      const html = source`
+        <div id="my-element">Hi there</div>
+      `
+
+      const test = source`
+        cy.get('#my-element')
+      `
+
+      cy.runExample(html, test)
+    })
   })
 
-  it('adds 2 todos', function () {
-    cy.get('.new-todo')
-      .type('learn testing{enter}')
-      .type('be cool{enter}')
-    cy.get('.todo-list li').should('have.length', 2)
-  })
+  describe('.then()', () => {
+    it('invokes a callback function with the current subject', () => {
+      const html = source`
+        <ul class="connectors-list">
+          <li>Walk the dog</li>
+          <li>Feed the cat</li>
+          <li>Write JavaScript</li>
+        </ul>
+        `
 
-  // more examples
-  //
-  // https://github.com/cypress-io/cypress-example-todomvc
-  // https://github.com/cypress-io/cypress-example-kitchensink
-  // https://on.cypress.io/writing-your-first-test
+      const test = source`
+        cy.get('.connectors-list > li')
+          .then(($lis) => {
+            expect($lis, '3 items').to.have.length(3)
+            expect($lis.eq(0), 'first item').to.contain('Walk the dog')
+            expect($lis.eq(1), 'second item').to.contain('Feed the cat')
+            expect($lis.eq(2), 'third item').to.contain('Write JavaScript')
+          })
+      `
+
+      cy.runExample(html, test)
+    })
+  })
 })
