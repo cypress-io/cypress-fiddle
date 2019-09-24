0a164d7f3683c1b28d9c9c6039dec7aa117dc3ee
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-09-23 22:59:59 -0400

    find fiddles in Markdown

---
+++ cypress/integration/text.md
@@ -0,0 +1,13 @@
+this is some test
+
+<!-- fiddle -->
+fiddle A 1
+fiddle A 2
+<!-- fiddle-end -->
+
+after fiddle
+
+<!-- fiddle -->
+fiddle B 1
+fiddle B 2
+<!-- fiddle-end -->
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,6 +6,39 @@ module.exports = (on, config) => {
     console.log({filePath, outputPath, shouldWatch})
     return new Promise((resolve, reject) => {
       const md = fs.readFileSync(filePath, 'utf8')
+      // console.log('----')
+      // console.log(md)
+      // console.log('----')
+
+      const lines = md.split('\n')
+      const fiddles = []
+
+      let start
+      do {
+        start = lines.indexOf('<!-- fiddle -->', start)
+        if (start === -1) {
+          break
+        }
+        const end = lines.indexOf('<!-- fiddle-end -->', start)
+        if (end === -1) {
+          break
+        }
+
+        const fiddle = lines.slice(start + 1, end).join('\n')
+        // console.log('found fiddle')
+        // console.log('----')
+        // console.log(fiddle)
+        // console.log('----')
+        fiddles.push(fiddle)
+
+        start = end + 1
+      } while (true)
+
+      // const fiddleRegex = /<!-- fiddle -->\n()<!-- fiddle-end -->\n/
+      // const matches = fiddleRegex.exec(md)
+      // console.log('matches')
+      // console.log(matches)
+      console.log('found %d fiddles in file %s', fiddles.length, filePath)
 
       fs.writeFileSync(outputPath, '', 'utf8')
       resolve(outputPath)
